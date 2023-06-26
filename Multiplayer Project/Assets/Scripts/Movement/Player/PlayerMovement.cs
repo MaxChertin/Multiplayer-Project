fc6cc11adce481f5209a0f33a56197fe1a029e2a
MaxChertin <chertin.max@gmail.com>
2023-06-26 21:18:00 +0300

    Updated Client prediction + reconciliation

--- Multiplayer Project/Assets/Scripts/Movement/Player/PlayerMovement.cs
+++ Multiplayer Project/Assets/Scripts/Movement/Player/PlayerMovement.cs
@@ -13,85 +13,12 @@ public class PlayerMovement : NetworkBehaviour
     [SerializeField] [Range(-9.81f, -39.24f)] private float gravity = -29.43f;
     [SerializeField] private float groundDistance = 0.3f;
 
-    [SerializeField] private UIManager ui;
-
     private Vector3 velocity;
     private float x, z;
     private bool grounded;
-    
-    private void FixedUpdate()
-    {
-        if (!isLocalPlayer) return;
-        
-        // Input
-        // Vector2 input = ReadInput();
-        //CmdMoveOnServer(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"), Input.GetButton("Jump"));
 
-        /*    // Jumping
-            grounded = Physics.CheckSphere(groundCheck.position, groundDistance, whatIsGround);
-            if (grounded && velocity.y < 0)
-            {
-                velocity.y = -2f;
-            }
-            if (Input.GetButton("Jump") && grounded)
-            {
-                velocity.y = Mathf.Sqrt(-2f * gravity);
-            }
-            
-            // Basic movement
-            Vector3 moveDir = transform.right * x + transform.forward * z;
-            controller.Move(Time.deltaTime * speed * moveDir);
-            
-            // Gravity movement
-            velocity.y += gravity * Time.deltaTime;
-            controller.Move(velocity * Time.deltaTime);  */
-    }
-    
-    private void Update()
-    {
-        //Debug.Log(this.gameObject.transform.position);
-        
-        //if (!isLocalPlayer) return;
-        //Movement(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"), Input.GetButton("Jump"));
-    }
-
-    //[Command]
-    //private void CmdMoveOnServer(float x, float y, bool jump)
-    //{
-    //    Movement(x, y, jump);
-    //}
-    
-    // Testing
-    private void Movement(/*ClientPrediction.PlayerState previousState, */float x, float z, bool jump)
-    {
-        Debug.Log("In Movement");
-        // Jumping
-        grounded = Physics.CheckSphere(groundCheck.position, groundDistance, whatIsGround);
-        if (grounded && velocity.y < 0)
-        {
-            velocity.y = -2f;
-        }
-        if (jump && grounded)
-        {
-            velocity.y = Mathf.Sqrt(-2f * gravity);
-        }
-        
-        // Basic movement
-        Vector3 moveDir = transform.right * x + transform.forward * z;
-        controller.Move(Time.deltaTime * speed * moveDir);
-        
-        // Gravity movement
-        velocity.y += gravity * Time.deltaTime;
-        controller.Move(velocity * Time.deltaTime);
-
-        /*return new ClientPrediction.PlayerState
-        {
-            timestamp = previousState.timestamp + 1,
-            position = controller.transform.position,
-        };*/
-    }
-    
-    public ClientPrediction.GameState CalculateMovement(ClientPrediction.PlayerInput playerInput)
+    // Based on our input and our previous state, calculate the new state
+    public ClientPrediction.GameState CalculateMovement(ClientPrediction.GameState previousState, ClientPrediction.PlayerInput playerInput)
     {
         float x = playerInput.input.x, z = playerInput.input.y;
         bool jump = playerInput.isJumping;
@@ -118,7 +45,7 @@ public class PlayerMovement : NetworkBehaviour
         return new ClientPrediction.GameState
         {
             usedInput = playerInput,
-            position = controller.transform.position,
+            position = controller.transform.position// + previousState.position,
         };
     }
 }
--- Multiplayer Project/Assets/Scripts/Networking/ClientPrediction.cs
+++ Multiplayer Project/Assets/Scripts/Networking/ClientPrediction.cs
@@ -1,4 +1,3 @@
-using System;
 using System.Collections.Generic;
 using Mirror;
 using UnityEngine;
@@ -6,6 +5,7 @@ using UnityEngine;
 public class ClientPrediction : NetworkBehaviour
 {
     [SerializeField] private PlayerMovement playerMovement;
+    [SerializeField] private CharacterController cc;
     
     public struct PlayerInput
     {
@@ -25,8 +25,8 @@ public class ClientPrediction : NetworkBehaviour
     private PlayerInput lastInput;
     private GameState currentPredictedState;
 
-    [SyncVar(hook = nameof(OnStateReceive))] 
-    private GameState serverState;
+    [SyncVar(hook = nameof(OnStateReceive))]
+    public GameState serverState;
 
     private void Awake()
     {
@@ -56,22 +56,30 @@ public class ClientPrediction : NetworkBehaviour
                 input = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical")),
                 isJumping = Input.GetButton("Jump"),
             };
-            // Send input to server
-            CmdSendInputToServer(currentInput);
-            // Store the input in the list
-            moveMemory.Add(currentInput);
-            // Localy calculate movement
-            currentPredictedState = playerMovement.CalculateMovement(currentInput);
 
-            lastInput = currentInput;
+            if (currentInput.input != Vector2.zero)
+            {
+                lastInput = currentInput;
+                // Send input to server
+                CmdSendInputToServer(currentInput);
+                // Store the input in the list
+                moveMemory.Add(currentInput);
+                // Localy calculate movement
+                UpdateCurrentPredictedState();
+            }
         }
+        
+        SyncState();
+        // Syncing between client and server state
+        //SyncState();
+        
     }
 
     // Call calculateMovement ONLY from server.
     [Command]
     private void CmdSendInputToServer(PlayerInput playerInput)
     {
-        serverState = playerMovement.CalculateMovement(playerInput);
+        serverState = playerMovement.CalculateMovement(serverState, playerInput);
         Debug.Log($"calculated new state for player, syncing in progress... stateTimestamp={serverState.usedInput.timestamp}, statePos={serverState.position}");
     }
 
@@ -79,5 +87,38 @@ public class ClientPrediction : NetworkBehaviour
     {
         serverState = newState;
         Debug.Log($"Got new state from server! (stateTimestamp={serverState.usedInput.timestamp}, statePos={serverState.position})");
+        if (moveMemory != null)
+        {
+            while (moveMemory.Count > currentPredictedState.usedInput.timestamp - serverState.usedInput.timestamp) {
+                moveMemory.RemoveAt(0);
+            }
+        
+            UpdateCurrentPredictedState();
+        }
+    }
+
+    private void UpdateCurrentPredictedState()
+    {
+        currentPredictedState = serverState;
+        
+        foreach (PlayerInput input in moveMemory)
+        {
+            currentPredictedState = playerMovement.CalculateMovement(currentPredictedState, input);
+            Debug.Log($"Calculating predictedState based on server state, predictedState -> {currentPredictedState.position} (used input: {input.input},{input.timestamp})");
+        }
+    }
+
+    private void SyncState()
+    {
+        if (isServer)
+        {
+            transform.position = serverState.position;
+            return;
+        }
+
+        //  if (!isLocalPlayer) return;
+      
+        transform.position = Vector3.Lerp(transform.position, currentPredictedState.position, 0.2f);
+        //Debug.Log($"Syncing states, current state: {transform.position}, predicted state: {currentPredictedState.position}");
     }
 }
