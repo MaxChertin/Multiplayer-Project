a080f811d6c23d3a4b663e8e5fc29229795568ba
MaxChertin <chertin.max@gmail.com>
2023-07-06 23:54:43 +0300

    Added Blend Tree Animations

--- Multiplayer Project/Assets/Scripts/Animator/PlayerAnimationManager.cs
+++ Multiplayer Project/Assets/Scripts/Animator/PlayerAnimationManager.cs
@@ -3,7 +3,8 @@ using UnityEngine;
 
 public class PlayerAnimationManager : MonoBehaviour
 {
-    [SerializeField] private Animator animator;
+    [SerializeField] private Animator FP_animator;
+    [SerializeField] private Animator CHARACTER_animator;
     [SerializeField] private PlayerMovement playerMovement;
     // Reference to input sys
     [SerializeField] private PlayerInput input;
@@ -31,11 +32,11 @@ public class PlayerAnimationManager : MonoBehaviour
         isGrounded = playerMovement.grounded;
 
         if ((x != 0 || z != 0) && isGrounded) {
-            animator.SetInteger(MOVEMENT_WALK, isRunning ? MOVEMENT_RUN : MOVEMENT_WALK);
+            FP_animator.SetInteger(MOVEMENT_WALK, isRunning ? MOVEMENT_RUN : MOVEMENT_WALK);
         }
         
         else if (x == 0 && z == 0)
-            animator.SetInteger(MOVEMENT_INDEX, MOVEMENT_NO_MOVEMENT);
+            FP_animator.SetInteger(MOVEMENT_INDEX, MOVEMENT_NO_MOVEMENT);
     }
 
     public void FootStepSoundLocalEvent()
